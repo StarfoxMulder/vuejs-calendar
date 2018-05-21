fd3bcc295f58dd7e78f85d14e666872237de319f
StarfoxMulder <cjprestia@gmail.com>
2018-05-20 21:56:16 -0400

    wrapping it up for the night

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 Vue.use(Vuex);
 import moment from "moment-timezone";
+import axios from "axios";
 
 export default new Vuex.Store({
   state: {
@@ -41,10 +42,12 @@ export default new Vuex.Store({
       state.eventFormActive = payload;
     },
     addEvent(state, payload) {
-      state.events.push({
+      let obj = {
         description: payload,
         date: state.eventFormDate
-      });
+      };
+      state.events.push(obj);
+      axios.post("/add_event", obj);
     },
     eventFormDate(state, payload) {
       state.eventFormDate = payload;
